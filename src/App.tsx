294910c5fbe91f80644e272dab3d3ecd7d0e7c8f
muhammaduxair <uxairjaaan@gmail.com>
2021-03-25 14:40:57 +0500

    taskbox app with storybook

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskBox from "./components/TaskBox";
+
+function App() {
+  return (
+    <div className="App">
+      <TaskBox />
+    </div>
+  );
+}
+
+export default App;
